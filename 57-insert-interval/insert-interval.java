2c5a8f53ed63d1a2fa573209a97556320d7158d0
prithvikota31 <prithvikota@microsoft.com>
2026-09-13 20:46:27 -0700

    Time: 1 ms (97.95%) | Memory: 47.3 MB (7.14%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,35 +1,37 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int n = intervals.length;
+        List<int[]> finalList = new ArrayList<>();
 
-        List<int[]> ans = new ArrayList<>();
+        int ind = 0;
 
-        int index = 0;
-
-        int mergedStart = newInterval[0];
-        int mergedEnd = newInterval[1];
-
-        while(index < n && intervals[index][1] < mergedStart)
+        while(ind < intervals.length && intervals[ind][1] < newInterval[0])
         {
-            ans.add(new int[]{intervals[index][0], intervals[index][1]});
-            index++;
+            finalList.add(intervals[ind]);
+            ind++;
+            
         }
 
-        while(index < n && intervals[index][1] >= mergedStart && intervals[index][0] <= mergedEnd)
+        //now we found overlap
+        int[] merged = newInterval;
+
+        //while there's overlap start merging
+        while(ind < intervals.length && intervals[ind][0] <= merged[1])
         {
-            mergedStart = Math.min(mergedStart, intervals[index][0]);
-            mergedEnd = Math.max(mergedEnd, intervals[index][1]);
-            index++;
+            merged[0] = Math.min(merged[0], intervals[ind][0]);
+            merged[1] = Math.max(merged[1], intervals[ind][1]);
+            ind++;
         }
 
-        ans.add(new int[]{mergedStart, mergedEnd});
+        finalList.add(merged);
 
-        while(index < n)
+
+        while(ind < intervals.length)
         {
-            ans.add(new int[]{intervals[index][0], intervals[index][1]});
-            index++;
+            finalList.add(intervals[ind]);
+            ind++;
         }
 
-        return ans.toArray(new int[0][]);
+        return finalList.toArray(new int[0][]);
+
     }
 }
